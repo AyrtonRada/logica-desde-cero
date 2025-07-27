90a1f489a5d2bdf6734d8b09774aa134830dc732
Ayrton <Ayrton@DESKTOP-HE4TFT9.tigo.net.bo>
2025-07-26 23:56:37 -0400

    Ejercicio 00 - AyrtonRada.java

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/AyrtonRada.java
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/AyrtonRada.java
@@ -1,18 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+/*Esto es un Comentario
+en varias lineas*/
+
+//Esto es un Comentario en una sola linea
+
+/** esto sirve para documentar clases, metodos y atributos.
+ * 
+ * 
  */
 
-/**
- *
- * @author Ayrton
- */
-public class AyrtonRada {
+//https://www.java.com/es/
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        // TODO code application logic here
+//VARIABLES
+public class AyrtonRada{
+    public static void main(String[] args){
+        //VARIABLES
+        int num = 32; //numero entero estandar
+        long num2 = 160; // numero entero grande
+        float num3 = 1.3f; //numero decimal simple
+        double num4 = 1.3156; //numero decimal doble (no lleva la f)
+        char num5 = 'A'; //comillas simples para 1 solo caracter
+        boolean num6 = true; //valor logico
+        String num7 = "HOLA"; //cadena de caracteres
+        
+        //CONTANTES
+        //se debe escribir la palabra "final" al inicio 
+        final double PI = 3.1416;
+        final int max_usuarios = 100;
+        System.out.print("Hola, Java");
     }
+    
+
 }
